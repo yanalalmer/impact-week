8a87c4774333d1771ecc477401b50617df5815a1
robertkwasny <robertadamkwasny@gmail.com>
2018-11-29 14:35:37 +0100

    add codeigniter template

---
+++ application/views/main.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet"
+    href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+    integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO"
+    crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" link="/assets/styles.css">
+  </head>
+  <body>
+    <div class="container">
+      <div class="row">
+      
+      </div>
+    </div>
+  </body>
+</html>
