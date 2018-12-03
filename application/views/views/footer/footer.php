e60aa92b0ac0c99ba014f48e6cc26f34a35ae278
Wesamsh14 <38855201+Wesamsh14@users.noreply.github.com>
2018-12-03 16:15:03 +0100

    Add files via upload

---
+++ application/views/views/footer/footer.php
@@ -0,0 +1,66 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+		<link rel="stylesheet" href="/assets/style-footer.css">
+    <title>Footer</title>
+  </head>
+  <body>
+      <footer class="content-info">
+      	<div class="container">
+        		<div class="row">
+        			<div class="col-md-3 widget text-4 widget_text">
+                <h4>Contact<p></p></h4>
+            			<div class="textwidget">
+                    <p><a href="mailto:team@femaleventures.nl" target="_blank" rel="noopener">team@femaleventures.nl</a></p>
+                      <p>&nbsp;</p>
+                      <p>&nbsp;</p>
+                  </div>
+        		 </div>
+             <div class="col-md-3 widget text-3 widget_text">
+               <h4>Follow us<p></p></h4>
+               <div class="textwidget">
+                  <p>
+                   <span class="fab fa-linkedin"></span>
+                     <a href="https://www.linkedin.com/organization/14974484/" target="_blank" rel="noopener">Link us on Linkedin</a><br />
+                   <span class="fab fa-facebook-f"></span>
+                       <a href="https://www.facebook.com/femaleventures/" target="_blank" rel="noopener">Like us on Facebook</a><br />
+                   <span class="fab fa-twitter"></span>
+                     <a href="https://twitter.com/FemVentures" target="_blank" rel="noopener">Follow us on Twitter</a>
+                  </p>
+                </div>
+        		 </div>
+             <div class="col-md-3 widget mailpoet_form-2 widget_mailpoet_form">
+        			  <h4>Subscribe to Our Newsletter</h4>
+        	      <div id="mailpoet_form_2" class="mailpoet_form mailpoet_form_widget">
+        			    <label class="mailpoet_text_label">Email<span class="mailpoet_required"> * </span></label><br>
+                  <input type="email" class="mailpoet_text" name="data[form_field]" title="Email" value="" data-automation-id="form_email"/></p>
+                    <p class="mailpoet_paragraph">
+                  <input type="submit" class="mailpoet_submit" value="Subscribe!" /></p>
+         	      </div>
+        	   </div>
+        		 <div class="col-md-3 widget nav_menu-2 widget_nav_menu">
+               <h4>Information<p></p></h4>
+               <div class="menu-footer-menu-container">
+                 <ul id="menu-footer-menu" class="menu">
+                   <li id="menu-item-81" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-81"><a href="https://femaleventures.nl/terms-and-conditions/">Terms and Conditions</a></li>
+                   <li id="menu-item-1339" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1339"><a href="https://femaleventures.nl/privacy-main/">Privacy</a></li>
+                   <li id="menu-item-242" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-242"><a href="">Logout</a></li>
+                 </ul>
+               </div>
+             </div>
+          </div>
+      	</div>
+      </footer>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+  </body>
+</html>
---
+++ application/views/views/profile.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Your profile</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <style media="screen">
+
+    </style>
+    <?php
+    if( ! $this->session->user) {
+      redirect('/users');
+      die();
+    }
+    ?>
+  </head>
+  <body>
+
+
+    <?php
+    if ($this->session->profile_edit_status == FALSE ) {
+      ?>
+      <a href="/users/edit_profile">Edit your profile</a>
+
+      <p>
+        First name: <?=$this->session->user['first_name']?>
+      </p>
+
+      <p>
+        Last name: <?=$this->session->user['last_name']?>
+      </p>
+
+      <p>
+        Email: <?=$this->session->user['email']?>
+      </p>
+
+      <p>
+        City: <?=$this->session->user['city']?>
+      </p>
+    </div>
+      <?php
+    } else
+    {
+      ?>
+      <form action="/users/edit_profile" method="post">
+        <label>First Name</label>
+        <input type='text' name='first_name' value=<?=$this->session->user['first_name']?>>
+        <br>
+        <label>Last name</label>
+        <input type='text' name='last_name' value=<?=$this->session->user['last_name']?>>
+        <br>
+        <label>email</label>
+        <input type='email' name='email' value=<?=$this->session->user['email']?>>
+        <br>
+        <!--
+        Later: add more fields here.
+        -->
+        <input type='submit' value='Submit' name='submit_profile_edit'>
+      </form>
+      <?php
+    }
+    ?>
+
+  <?php
+  var_dump($this->session->all_userdata());
+    ?>
+
+
+  </body>
+</html>
